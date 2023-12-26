2043bf216dfccdb9d455988f2786c20a10820cbf
er <er@ubuntu-console.(none)>
2023-12-26 11:58:09 -0500

    Addition: route tables, rt associations

--- 03-subnets.tf
+++ 03-subnets.tf
@@ -31,3 +31,54 @@ resource "aws_subnet" "private_db_subnet" {
     Name = join("-", [local.private_db_subnet_name, var.az_names[count.index]])
   }
 }
+
+
+## Route table for public
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    Name = local.public_route_table_name
+  }
+}
+
+
+## Route table for Private
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = local.private_route_table_name
+  }
+}
+
+## route table associations
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_db_rt_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private_db_subnet[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
